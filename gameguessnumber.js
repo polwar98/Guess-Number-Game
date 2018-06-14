e278bec08891df0ae4d823ecf2aa9930fa395683
Szymon Szewc <32969418+polwar98@users.noreply.github.com>
2018-06-15 01:03:49 +0200

    Add files via upload

--- gameguessnumber.js
+++ gameguessnumber.js
@@ -1,13 +1,17 @@
-var numToGuess = Math.floor((Math.random() * 1000) + 1);
+const numToGuess = Math.floor((Math.random() * 1000) + 1);
 
-var turnCounter = 0;
+let turnCounter = 0;
 let nrClass;
 
-$('.logs').html(numToGuess+"<br>");
+let min = 0;
+let max = 1001;
 
-$(document).ready(function(){
+// $('.logs').html(numToGuess+"<br>");
+
+$(document).ready(function()
+{
    $("#guessForm").focus();
-});
+})
 
 function isNumber()
 {
@@ -18,110 +22,104 @@ function isNumber()
     else
         {
         $('.logs').append('Wprowadź prawidłową liczbę! <br>');
+        scrollBottom();
         }
 }
 function checkNumber()
 {
     let guess = document.getElementById("guessForm").value;
     let difference = Math.abs(numToGuess-guess);
-    let min = 0;
-let max = 1001;
+
 
 
     colorText(difference, nrClass);
-//    console.log(difference);
-//    console.log(nrClass);
 
-    
-    if (numToGuess==guess)
-        {
-            turnCounter++;
-            $('.logs').append('<a class=sq'+nrClass+'a>To twoja '+turnCounter+' próba. Gratulacje wygrałeś!</a><br>');
-            $('.logs').before('<span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>');
-            
-        }
+    if (guess > 1000 || guess < 1)
+    {
+        $('.logs').append('Wprowadź prawidłową liczbę z zakresu 1-1000! <br>');
+        scrollBottom();
+    }
+    else if (numToGuess==guess)
+    {
+        turnCounter++;
+        $('.logs').append('<a class=sq'+nrClass+'a>To twoja '+turnCounter+' próba. Gratulacje wygrałeś!</a><br>');
+        $('.logs').before('<span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>');
+        $('.form').hide();
+        scrollBottom();
+    }
     else if(numToGuess>guess)
-        {
-            turnCounter++;
-            intervalMin(min, guess);
-            min = window.min;
-            $('.logs').append('<a class=sq'+nrClass+'a>To twoja '+turnCounter+' próba. Jestem gdzieś tutaj '+min+'-'+max+'. To za mało! Próbuj dalej!</a><br>');
-        }
+    {
+        turnCounter++;
+        intervalMin(min, guess);
+        min = window.min;
+        let nrClassAdd1 = nrClass + 1;
+        $('.logs').append('<a class=sq'+nrClass+'a>'+nrClassAdd1+'. To twoja '+turnCounter+' próba. To za mało! Jestem gdzieś tutaj '+min+'-'+max+'. Próbuj dalej!</a><br>');
+        scrollBottom();
+    }
     else
-        {
-            turnCounter++;
-            intervalMax(max, guess, min);
-            max = window.max;
-            $('.logs').append('<a class=sq'+nrClass+'a>To twoja '+turnCounter+' próba. Jestem gdzieś tutaj '+min+'-'+max+'. To za dużo! Próbuj dalej!</a><br>');
-        }
+    {
+        turnCounter++;
+        intervalMax(max, guess);
+        max = window.max;
+        let nrClassAdd1 = nrClass + 1;
+        $('.logs').append('<a class=sq'+nrClass+'a>'+nrClassAdd1+'. To twoja '+turnCounter+' próba. To za dużo! Jestem gdzieś tutaj '+min+'-'+max+'. Próbuj dalej!</a><br>');
+        scrollBottom();
+    }
+}
+function scrollBottom()
+{
+var mydiv = $(".logs");
+mydiv.scrollTop(mydiv.prop("scrollHeight"));
+}
+function intervalMin(min, guess)
+{
+    if (guess>min)
+    {
+        window.min=guess;
+        return min;
+    }
+}
+function intervalMax(max, guess)
+{
+    if (guess<max)
+    {
+        window.max=guess;
+        return max;
+    }
 }
 function colorText(difference)
 {
-    if(difference>500)
-        {
-            nrClass=0;
-            return;
-        }
-    else if(250<difference && difference<501)
-        {
-            nrClass=1;
-            return;
-        }
-    else if(120<difference && difference<251)
-        {
-            nrClass=2;
-            return;
-        }
-    else if(50<difference && difference<121)
-        {
-            return nrClass=3;
-        }
-    else if(30<difference && difference<51)
-        {
-            return nrClass=4;
-        }
-    else if(15<difference && difference<31)
-        {
-            return nrClass=5;
-        }
-    else if(5<difference && difference<16)
-        {
-            return nrClass=6;
-        }
-    else if(1<difference && difference<6)
-        {
-            return nrClass=7;
-        }
-    else
-        {
-            nrClass=8;
-            return nrClass;
-        }
-}
-function intervalMin(min, guess, max)
-{
-    if (guess>min)
-        {
-            window.min=guess;
-            console.log(min);
-            return min;
-        }
-    else
-        {
-            return min;
-        }
-}
-function intervalMax(max, guess, min)
-{
-    if (guess<max)
-        {
-            window.max=guess;
-            console.log(max);
-            return max;
-        }
-    else
-        {
-            return max;
-        }
-}
+    if(difference>250)
+    {
+        return nrClass=0;
+    }
+    else if(121<=difference && difference<=250)
+    {
+        return nrClass=1;
+    }
+    else if(61<=difference && difference<=120)
+    {
+        return nrClass=2;
+    }
+    else if(26<=difference && difference<=60)
+    {
+        return nrClass=3;
+    }
+    else if(11<=difference && difference<=25)
+    {
+        return nrClass=4;
+    }
+    else if(5<=difference && difference<=10)
+    {
+        return nrClass=5;
+    }
+    else if(1<=difference && difference<=4)
+    {
+        return nrClass=6;
+    }
+    else 
+    {
+        return nrClass=7;
+    }
 
+}
